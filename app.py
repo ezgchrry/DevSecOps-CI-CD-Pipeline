4ecd1eb685753270d5d6731304707fc4aec6655c
Elif Ezgi Umulğan <147320461+ezgchrry@users.noreply.github.com>
2026-04-29 14:48:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #bu dosya "ürünümüzü" temsil etmektedir.
 #kodumuz kullanıcının girdiği şifrenin uzunluğunu kontrol ediyor. eğer 8 karakterden büyükse güçlü, küçükse zayıf sonucunu dönüyor.
 #fonksiyonel gereksinimler burada tanımlandı.
-# ezgi 
 def check_password_strength(password):
     if len(password) >= 8:
         return "Güçlü"
@@ -9,3 +8,5 @@ def check_password_strength(password):
 
 if __name__ == "__main__":
     print(check_password_strength("12345678"))
+
+os.system("echo Sunum Basarili")
